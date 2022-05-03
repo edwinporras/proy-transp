12c6cd5c130c90a14ab1dd2f0b2e26a2c485cfb9
edwinporras <69282427+edwinporras@users.noreply.github.com>
2022-05-03 16:01:44 -0500

    Update content v1.0.1

---
+++ error404.php
@@ -0,0 +1,40 @@
+<?php 
+  include_once "php/header.php";
+?> 
+<style>
+  #home-video-play{
+    display:none;
+  }
+  .home{
+    height:0vh;
+  }
+
+</style>
+ 
+<body>
+    <div>
+      <div class="cont_error404main">
+        <div class="cont_main_error">
+          <div class="cont_int_err cont_err1">
+            <!-- <img class="img_err2" src="./ima_act/Logo-Cirud-png.png" alt=""> -->
+          </div>
+          <div class="cont_int_err cont_err2">
+            <div class="cont_int_er1">
+      
+            </div>
+            <div class="cont_int_er2">
+              <img class="img_err" src="./ima_act/404.png" alt="">
+            </div>
+            
+          </div>
+          <div class="cont_int_err cont_err3">
+            <h3 class="titulo_er_main">OOPSS!</h3>
+            <h4 class="titulo_er_alt"><span class="tit_alt_er">Disc&uacute;lpanos</span>, estamos<br> presentando problemas en<br> estos momentos</h4>
+            <a class="btn_err btn btn-danger" href="index">INICIO</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  <?php 
+    include_once "php/footer.php";
+  ?>
---
+++ php/header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<!--
+	Salient by TEMPLATE STOCK
+	templatestock.co @templatestock
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html lang="en">
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Salient - Free Responsive HTML5 Template | Template Stock</title>
+
+  <!-- Custom Google fonts -->
+  <link href='http://fonts.googleapis.com/css?family=Raleway:400,500,300,700' rel='stylesheet' type='text/css'>
+  <link href="http://fonts.googleapis.com/css?family=Crimson+Text:400,600" rel="stylesheet" type="text/css">
+  <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600" rel="stylesheet" type="text/css">
+
+  <!-- Bootstrap CSS Style -->
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+  <!-- Template CSS Style -->
+  <link rel="stylesheet" href="assets/css/style.css">
+
+  <!-- Animate CSS  -->
+  <link rel="stylesheet" href="assets/css/animate.css">
+
+  <!-- FontAwesome 4.3.0 Icons  -->
+  <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+  <!-- et line font  -->
+  <link rel="stylesheet" href="assets/css/et-line-font/style.css">
+
+  <!-- BXslider CSS  -->
+  <link rel="stylesheet" href="assets/css/bxslider/jquery.bxslider.css">
+
+  <!-- Owl Carousel CSS Style -->
+  <link rel="stylesheet" href="assets/css/owl-carousel/owl.carousel.css">
+  <link rel="stylesheet" href="assets/css/owl-carousel/owl.theme.css">
+  <link rel="stylesheet" href="assets/css/owl-carousel/owl.transitions.css">
+
+  <!-- Magnific-Popup CSS Style -->
+  <link rel="stylesheet" href="assets/css/magnific-popup/magnific-popup.css">
+  <link rel="stylesheet" href="./assets/css/slide.css">
+
+  <!-- font-awesome CDN Icons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+	
+</head>
+<body>
